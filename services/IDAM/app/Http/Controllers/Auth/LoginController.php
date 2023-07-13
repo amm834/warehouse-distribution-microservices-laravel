79c98038b1d4d4c7873e65543e016e9c85d63549
Aung Myat Moe <aungmyatmoe834@gmail.com>
2023-07-13 10:32:17 +0630

    feat: add IDAM

---
+++ services/IDAM/app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(LoginRequest $request)
+    {
+        // register user
+        // validate request
+        // store in redis
+    }
+}
---
+++ services/IDAM/app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\RegisterRequest;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(RegisterRequest $request)
+    {
+        // register user
+        // validate request
+        // store in redis
+    }
+}
